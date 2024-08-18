e36f0a6ccd20cee65760754df6abd58b8f5628fd
marcin.mrugas <marcin.mrugas@allegro.com>
2024-08-18 20:24:49 +0200

    Add day template

---
+++ day_template.py
@@ -0,0 +1,35 @@
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2017, day=-1)
+
+
+def parse(input_data):
+    return input_data.split("\n")
+
+
+def part1(input_data):
+    data = parse(input_data)
+    return -1
+
+
+def part2(input_data):
+    data = parse(input_data)
+    return -1
+
+
+def main():
+    assert 0 == part1(puzzle.examples[0].input_data)
+    print("part1 example OK")
+
+    puzzle.answer_a = part1(puzzle.input_data)
+    print("part1 OK")
+
+    assert 0 == part2(puzzle.examples[0].input_data)
+    print("part2 example OK")
+
+    puzzle.answer_b = part2(puzzle.input_data)
+    print("part2 OK")
+
+
+if __name__ == '__main__':
+    main()
